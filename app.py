98573802a1f73748b094124c5b4bcd086f695496
leizhangbu <leizhang@bu.edu>
2020-09-08 22:34:03 -0400

    aaa

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-
+        app.vars={}
         #request was a post
         app.vars['ticker'] = request.form['ticker']
         app.vars['results'] = ap.get_data(app.vars['ticker'])
