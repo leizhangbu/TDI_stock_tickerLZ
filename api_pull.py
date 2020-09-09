94d895472efd77d13d6568e7d9398d96ec53fac2
leizhangbu <leizhang@bu.edu>
2020-09-08 21:21:34 -0400

    aaa

--- api_pull.py
+++ api_pull.py
@@ -18,17 +18,13 @@ def get_data(ticker):
 
     
     json = resp.json()
-    print(json)
     df = pd.DataFrame.from_dict(json['Time Series (Daily)'],orient='index')
-    print('1',df[:10])
     df ['date'] = pd.to_datetime(df.index)
-    print('2',df[:10])
     datatable = df ['date']
-    print('3',df[:10])
     df['4. close'].astype('float')
     print('4',df[:10])
     try:
-        df.index = pd.to_datetime(df.index)
+        df.index = pd.to_datetime(df.date)
         print('5',df[:10])
         print(df[start_date:today]['4. close'])
         return df[start_date:today]['4. close']
